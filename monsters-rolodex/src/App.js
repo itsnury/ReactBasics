65aa4384248bb50770188dc686d553f6783e79d3
Nury Kim <nkim@unb.ca>
2023-05-11 15:02:44 -0300

    learning basics

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,39 @@
+import { Component } from 'react';
+/*
+component class allows us to turn functional components
+to class component
+*/
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // consructor always runs first when App is called
+  constructor() {
+    super(); // overrides methods of superclass Component
+
+    this.state = {
+      name: 'Nury'
+    } // state is always a json object
+  }
+
+  // explicitly telling our class component to render app
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button 
+            onClick={() => {
+              this.setState({name:'Devin'})
+            }}
+          >
+          Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
